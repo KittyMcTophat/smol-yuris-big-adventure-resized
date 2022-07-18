ea35185096e84c9fed1253e27ef1649ca1c5ec06
Kitty <82963444+KittyMcTophat@users.noreply.github.com>
2022-07-17 18:12:04 -0600

    Worked on Enemies
    - Enemies have a death sound now
    - Enemies now give coins when killed

--- Scripts/Actors/Enemy.gd
+++ Scripts/Actors/Enemy.gd
@@ -2,7 +2,9 @@ extends PhysicsActor
 
 class_name Enemy
 
+export(NodePath) onready var projectile_spawner = get_node_or_null(projectile_spawner);
 export var health : int = 1;
+export var reward_money : int = 25;
 
 func hurt(amount : int):
 	health -= amount;
@@ -10,4 +12,16 @@ func hurt(amount : int):
 		_kill();
 
 func _kill():
+	allow_movement = false;
+	hide();
+	collision_layer = 0;
+	collision_mask = 0;
+	
+	if projectile_spawner != null && projectile_spawner.get("enabled"):
+		projectile_spawner.enabled = false;
+	
+	yield(get_tree().create_timer(0.1), "timeout");
+	Global.coin_counter.money += reward_money;
+	$Dead.play();
+	yield($Dead, "finished");
 	queue_free();
--- Scripts/Actors/ProjectileSpawner.gd
+++ Scripts/Actors/ProjectileSpawner.gd
@@ -1,5 +1,8 @@
 extends Spatial
 
+class_name ProjectileSpawner
+
+export var enabled : bool = true;
 export var velocity : float = 1.5;
 export var projectile : PackedScene = null;
 export var offset : Vector3 = Vector3(0.0, 0.0, -0.05);
@@ -9,6 +12,8 @@ export var delay : float = 2.0;
 var _time_since_last_projectile : float = 0.0;
 
 func _physics_process(delta):
+	if enabled == false:
+		return;
 	if time_based:
 		_time_since_last_projectile += delta;
 		if _time_since_last_projectile >= delay:
