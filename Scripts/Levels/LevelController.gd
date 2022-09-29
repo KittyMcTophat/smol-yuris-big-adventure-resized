a4501ec1d0ec807d6149e7d7b883fb4c8abceb9c
Kitty <82963444+KittyMcTophat@users.noreply.github.com>
2022-09-29 09:42:18 -0600

    Add show_coin_counter property to LevelController
    - Set it to false in the cutscenes and title screen

--- Scripts/Levels/LevelController.gd
+++ Scripts/Levels/LevelController.gd
@@ -4,6 +4,7 @@ class_name LevelController
 
 export var gravity : Vector3 = Vector3(0.0, -9.8, 0.0);
 export var music : AudioStream = null;
+export var show_coin_counter : bool = true;
 
 func _ready():
 	environment = load("res://level_env.tres");
@@ -12,3 +13,9 @@ func _ready():
 	# Sets the gravity equal to this level's gravity
 	PhysicsServer.area_set_param(get_viewport().world.space, PhysicsServer.AREA_PARAM_GRAVITY_VECTOR, gravity.normalized());
 	PhysicsServer.area_set_param(get_viewport().world.space, PhysicsServer.AREA_PARAM_GRAVITY, gravity.length());
+	if show_coin_counter == false:
+		Global.coin_counter.modulate = Color.transparent;
+
+func _notification(what):
+	if what == NOTIFICATION_PREDELETE:
+		Global.coin_counter.modulate = Color.white;
